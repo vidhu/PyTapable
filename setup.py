8709295abe2826ba46857536d34d592a00c31d80
vidhu <vidhu1911@gmail.com>
2020-05-23 00:01:31 -0400

    distribution

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pytapable",
+    version="0.0.1",
+    author="Vidhu Bhatnagar",
+    author_email="vidhu1911@gmail.com",
+    description="Provides utilities to implement a hookable interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vidhu/pytapable",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires='>=3.3',
+)
