08060a6eda02224c64a18251b33f51012a4cf83f
vidhu <vidhu1911@gmail.com>
2020-05-28 23:08:41 -0400

    fixup readme

--- pytapable/functional.py
+++ pytapable/functional.py
@@ -1,18 +1,42 @@
+from typing import Callable, Union, Optional, Any
 from functools import partial, wraps
 from .hooks import BaseHook, Tap, HookConfig
 
 
 class FunctionalTap(Tap):
     def __init__(self, name, fn, before, after):
+        """
+        Functional taps is used in combination with a :class:`~FunctionalBaseHook`
+
+        Args:
+            name (str): Name of the tap
+            fn (Callable): This will be called when the hook triggers
+            before (bool): If true, this tap will be called *before* the hooked function executes
+            after (bool): If true, this tap will be called *after* the hooked function executes
+        """
         super(FunctionalTap, self).__init__(name, fn)
         self.before = before
         self.after = after
 
 
-class FunctionalBaseHook(BaseHook):
+class FunctionalHook(BaseHook):
+    """
+    Functional hooks are created when :class:`~CreateHook` is used to decorate a class function. When a functional
+    hook is tapped, a :class:`~FunctionalTap` is created. Look at :func:`~FunctionalHook.call` to see how taps are
+    called
+    """
     HOOK_TYPE = BaseHook.FUNCTIONAL
 
     def tap(self, name, fn, before=True, after=True):
+        """
+        Creates a :class:`~FunctionalTap` for this hook
+
+        Args:
+            name (str): Name of the tap
+            fn (Callable): This will be called when the hook triggers
+            before (bool): If true, this tap will be called *before* the hooked function executes
+            after (bool): If true, this tap will be called *after* the hooked function executes
+        """
         tap = FunctionalTap(
             name=name,
             fn=fn,
@@ -23,13 +47,21 @@ class FunctionalBaseHook(BaseHook):
         self.taps.append(tap)
 
     def call(self, fn_args, is_before, fn_output=None):
+        """
+        Triggers all taps installed on this hook.
+
+        Args:
+            fn_args (dict): The arguments the hooked function was called with. `fn_args: { args: Tuple, kwargs: Dict }`
+            is_before (bool): True if the hook is being called after the hooked function has executed
+            fn_output (Optional[Any]): The return value of the hooked function if any. None otherwise
+        """
         for tap in self.taps:
             if (tap.before and is_before) or (tap.after and not is_before):
                 tap.fn(
                     fn_args=fn_args,
                     fn_output=fn_output,
                     context={
-                        'hook_type': FunctionalBaseHook.HOOK_TYPE,
+                        'hook_type': FunctionalHook.HOOK_TYPE,
                         'hook_type_label': self.label,
                         'tap_name': tap.name,
                         'is_before': is_before
@@ -38,6 +70,9 @@ class FunctionalBaseHook(BaseHook):
 
 
 class HookableMixin(object):
+    """
+    Mixin which instantiates all the decorated class methods. This is needed for decorated class methods
+    """
     def __init__(self, *args, **kwargs):
         super(HookableMixin, self).__init__(*args, **kwargs)
         self.hooks = {}
@@ -45,7 +80,7 @@ class HookableMixin(object):
         for method in map(partial(getattr, klass), dir(klass)):
             if hasattr(method, '_pytapable'):
                 hook_config = getattr(method, '_pytapable')
-                self.hooks[hook_config.name] = FunctionalBaseHook(
+                self.hooks[hook_config.name] = FunctionalHook(
                     interceptor=hook_config.interceptor
                 )
 
@@ -54,7 +89,11 @@ class HookableMixin(object):
 
 
 class CreateHook(object):
-    def __init__(self, name=None, interceptor=None):
+    """
+    Decorator used to creating Hooks on instance methods. It takes in name and optionally an instance of a
+     :class:`~pytapable.intercept_base.HookInterceptor`
+    """
+    def __init__(self, name, interceptor=None):
         self.name = name
         self.interceptor = interceptor
 
@@ -75,6 +114,3 @@ class CreateHook(object):
         hook_config = HookConfig(name=self.name, interceptor=self.interceptor)
         wrapper._pytapable = hook_config
         return wrapper
-
-    def get_name(self):
-        return
--- pytapable/hooks.py
+++ pytapable/hooks.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import Iterable, Optional
 from collections import namedtuple
 
 from .intercept_base import HookInterceptor
@@ -7,7 +8,17 @@ HookConfig = namedtuple('HookConfig', ['name', 'interceptor'])
 
 
 class Tap(object):
+    """
+    A Tap is an object created when you tap into a hook. It holds a reference to the function you want to
+    execute when the hook triggers
+    """
     def __init__(self, name, fn):
+        """
+
+        Args:
+            name (str): Name of the tap
+            fn (Callable): This will be called when the hook triggers
+        """
         self.name = name
         self.fn = fn
 
@@ -40,6 +51,35 @@ class BaseHook(object):
         return self.HOOK_TYPE_LABEL[self.HOOK_TYPE]
 
 
-def create_hook_name(name):
+def create_hook_name(name=""):
+    """
+    Utility to create a unique hook name. Optionally takes in a name. The output string is the name prefixed with a
+    UUID. This is useful to prevent collision in hook names when one class with hooks inherits hooks from another class
+
+    Example:
+        >>> create_hook_name()
+        >>> '7087eefc-8e94-4f0a-b7d3-453062bb7a34'
+        >>> create_hook_name('my_hook')
+        >>> '7087eefc-8e94-4f0a-b7d3-453062bb7a34:my_hook'
+
+    Args:
+        name (Optional[str]): Name of the hook
+    """
     prefix = str(uuid.uuid4())
     return "{}:{}".format(prefix, name)
+
+
+def create_hook_names(*names):
+    """
+    Useful shortcut to create multiple unique hook names in one statement
+    Example:
+        >>> HOOK_MY, HOOK_UNIQUE, HOOK_HOOK = create_hook_names('my', 'unique', 'hook')
+        >>> HOOK_ONE, HOOK_TWO, HOOK_THREE = create_hook_names(range(3))
+
+    Args:
+        *names: Argument of hook names
+    Returns:
+        Iterable: iterable which can be deconstructed across constants.
+    """
+    for name in names:
+        yield create_hook_name(name)
