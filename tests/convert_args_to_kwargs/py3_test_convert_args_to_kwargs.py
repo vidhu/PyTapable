a212a7a0eb56119cc3c90f13bf502590424a1379
vidhu <vidhu1911@gmail.com>
2020-06-21 14:30:34 -0400

    no qa py 2

--- tests/convert_args_to_kwargs/py3_test_convert_args_to_kwargs.py
+++ tests/convert_args_to_kwargs/py3_test_convert_args_to_kwargs.py
@@ -5,7 +5,7 @@ from pytapable.utils import merge_args_to_kwargs, get_arg_spec_py2_py3
 class TestMixedPy3(TestCase):
 
     def test_pep3102_ignore_args(self):
-        def fn(a, b, *, d):
+        def fn(a, b, *, d):  # noqa: E999
             pass
 
         kwargs = merge_args_to_kwargs(
